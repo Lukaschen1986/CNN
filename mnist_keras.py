df95f051ebc4329b9a5dfd93e00dae8668062c3e
Lukas_Chen <chen_wen_dong@126.com>
2017-11-14 17:12:29 +0800

    Update mnist_keras.py

--- mnist_keras.py
+++ mnist_keras.py
@@ -59,8 +59,8 @@ keep_prob = 0.8
 
 # 把类别0-9变成2进制，方便训练
 #one_hot = lambda y: np.eye(len(set(y)))[y]
-y_train = to_categorical(y_train, outNum)
-y_predict = to_categorical(y_predict, outNum)
+y_train_oh = to_categorical(y_train, outNum)
+y_predict_oh = to_categorical(y_predict, outNum)
 
 # model
 model = Sequential()
@@ -90,7 +90,9 @@ model.add(Conv2D(nb_filter=64,
                  kernel_initializer="random_uniform", 
                  bias_initializer="zeros",
                  kernel_regularizer=l2(0.0001)))
-model.add(BatchNormalization(center=True, beta_initializer='zeros', scale=True, gamma_initializer='ones'))
+model.add(BatchNormalization(center=True, beta_initializer='zeros', 
+                             scale=True, gamma_initializer='ones', 
+                             epsilon=10**-8))
 model.add(Activation("relu"))
 model.add(Dropout(rate=(1-keep_prob)))
 model.add(MaxPooling2D(pool_size=(2,2), border_mode="same"))
@@ -104,7 +106,9 @@ model.add(Conv2D(nb_filter=32,
                  kernel_initializer="random_uniform", 
                  bias_initializer="zeros",
                  kernel_regularizer=l2(0.0001)))
-model.add(BatchNormalization(center=True, beta_initializer='zeros', scale=True, gamma_initializer='ones'))
+model.add(BatchNormalization(center=True, beta_initializer='zeros', 
+                             scale=True, gamma_initializer='ones', 
+                             epsilon=10**-8))
 model.add(Activation("relu"))
 model.add(Dropout(rate=(1-keep_prob)))
 model.add(MaxPooling2D(pool_size=(2,2), border_mode="same"))
@@ -117,7 +121,9 @@ model.add(Dense(units=128,
                 kernel_initializer="random_uniform", # random_normal
                 bias_initializer="zeros",
                 kernel_regularizer=l2(0.0001)))
-model.add(BatchNormalization(center=True, beta_initializer='zeros', scale=True, gamma_initializer='ones'))
+model.add(BatchNormalization(center=True, beta_initializer='zeros', 
+                             scale=True, gamma_initializer='ones', 
+                             epsilon=10**-8))
 model.add(Activation("relu"))
 model.add(Dropout(rate=(1-keep_prob)))
 ## 6
@@ -127,28 +133,43 @@ model.add(Dense(units=128,
                 kernel_initializer="random_uniform", # random_normal
                 bias_initializer="zeros",
                 kernel_regularizer=l2(0.0001)))
-model.add(BatchNormalization(center=True, beta_initializer='zeros', scale=True, gamma_initializer='ones'))
+model.add(BatchNormalization(center=True, beta_initializer='zeros', 
+                             scale=True, gamma_initializer='ones', 
+                             epsilon=10**-8))
 model.add(Activation("relu"))
 model.add(Dropout(rate=(1-keep_prob)))
 ## 7
 model.add(Dense(units=outNum, activation='softmax'))
 ## summary
 model.summary()
-plot_model(model, to_file="model.png", show_shapes=True, show_layer_names=True, rankdir='TB')
+plot_model(model, to_file="model.png", show_shapes=True, show_layer_names=True)
 ## compile
 model.compile(loss="categorical_crossentropy", 
               optimizer=Adam(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=10**-8, decay=0.0),
               metrics=["accuracy"])
 ## fit
-model.fit(X_train_reshape, y_train, batch_size=batch_size, epochs=epochs, verbose=1)
+model.fit(X_train_reshape, y_train_oh, batch_size=batch_size, epochs=1, validation_split=0.2, verbose=1) #validation_data=(X,Y)
 ## pred
-y_pred = model.predict_classes(X_predict_reshape)
-#sum(y_pred == y_predict) / len(y_predict) # 0.98619
-
-#score = model.evaluate(X_predict, y_predict, verbose=0)
-#print('Test score:', score[0])
-#print('Test accuracy:', score[1])
-#y_pred = model.predict_classes(X_predict_reshape)
+loss, accu = model.evaluate(X_predict_reshape, y_predict_oh, verbose=1)
+output = model.predict(X_predict_reshape, batch_size=batch_size, verbose=1)
+y_pred = np.argmax(output, axis=1)
+sum(y_pred == y_predict) / len(y_predict) # 0.9249
+pd.crosstab(y_predict, y_pred, margins=True)
+'''
+col_0     0     1     2    3     4    5    6    7    8    9    All
+row_0                                                             
+0       975     1     1    0     2    0    1    0    0    0    980
+1         0  1135     0    0     0    0    0    0    0    0   1135
+2         6     6  1014    0     5    0    0    1    0    0   1032
+3         0    50     3  953     1    0    0    3    0    0   1010
+4         0     1     0    0   981    0    0    0    0    0    982
+5         2   112     0    4     8  740   12    1   12    1    892
+6        12    21     0    0    10    0  915    0    0    0    958
+7         1   158    10    0     9    0    0  850    0    0   1028
+8         3    12     1    2    12    0    1    2  941    0    974
+9         5    19     0    0   233    0    0    2    5  745   1009
+All    1004  1515  1029  959  1261  740  929  859  958  746  10000
+'''
 
 model.save(filepath="mnist-mpl.h5", overwrite=True, include_optimizer=True)
 model = load_model(filepath="mnist-mpl.h5", custom_objects=None, compile=True)
